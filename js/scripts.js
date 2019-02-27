fc8d0ced5893ad3baf29b504dea3e31877346ed6
Benedict Gattas (P0ntiff) <gattasb@gmail.com>
2019-02-27 20:16:59 +1000

    Navbar tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-$(window).scroll(function () {
-    if ($(document).scrollTop() > 50) {
-        $('nav').addClass('navbar-shrink');
-    } else {
-        $('nav').removeClass('navbar-shrink');
-    }
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $("nav").addClass("navbar-shrink");
+  } else {
+    $("nav").removeClass("navbar-shrink");
+  }
 });
